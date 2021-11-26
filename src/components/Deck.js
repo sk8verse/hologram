e2ff0540a2659cc996618337193aabdd275d0928
Beamer <beamer@hey.com>
2021-11-25 23:46:10 -0800

    WIP

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,108 +1,43 @@
-import React, { useRef } from "react"
-import { useGLTF, useTexture } from "@react-three/drei"
-import { useFrame } from "@react-three/fiber"
+import React, { useContext } from "react"
+import { useTexture } from "@react-three/drei"
+import SceneContext from "./SceneContext"
 
-const Deck = props => {
-  const { nodes } = useGLTF(
-    props.object || "https://sk8verse.s3.us-west-1.amazonaws.com/sk8board.gltf"
-  )
+const Deck = ({ getGeometry, ...props }) => {
+  const { objects } = useContext(SceneContext)
 
-  const boardTexture = useTexture(
+  const baseTexture = useTexture(
     "https://sk8verse.s3.us-west-1.amazonaws.com/sk8deck-wood.jpg"
   )
 
   const deckTexture = useTexture(
-    props.texture ||
+    objects?.deck?.texture ||
       `https://sk8verse.s3.us-west-1.amazonaws.com/sk8deck-wood.jpg`
   )
   deckTexture.flipY = false
 
-  const meshRotation = useRef()
-  const meshAngleRotation = useRef()
-
-  useFrame(({ clock }) => {
-    const elapsedTime = clock.getElapsedTime()
-    meshRotation.current.rotation.z = -elapsedTime * 1 + 10
-    meshAngleRotation.current.rotation.y = 1
-    meshAngleRotation.current.rotation.x = 2
-  })
-
-  const geo = key => nodes[key].geometry
-
-  const objects = props.objects || ["deck"]
+  const deckProps = { ...objects.deck, metalness: 0, roughness: 0.85 }
 
   return (
-    <group ref={meshAngleRotation}>
-      <group ref={meshRotation} {...props}>
-        {objects.includes("deck") && (
-          <group>
-            <mesh castShadow receiveShadow geometry={geo("deck-rim")}>
-              <meshStandardMaterial
-                metalness={0}
-                roughness={0.85}
-                map={boardTexture}
-                attach="material"
-                key="rim"
-                toneMapped={false}
-              />
-            </mesh>
-            <mesh castShadow receiveShadow geometry={geo("deck-wrap")}>
-              <meshStandardMaterial
-                metalness={0}
-                roughness={0.85}
-                map={deckTexture}
-                attach="material"
-                key="wrap"
-                toneMapped={false}
-              />
-            </mesh>
-          </group>
-        )}
-
-        <group position={[0, -10, 0]}>
-          {objects.includes("wheels") && (
-            <mesh castShadow receiveShadow geometry={geo("wheels")}>
-              <meshStandardMaterial
-                metalness={0}
-                roughness={0.85}
-                // map={deckTexture}
-                attach="material"
-                key="metal"
-                toneMapped={false}
-                color={0xe3d4ab}
-              />
-            </mesh>
-          )}
-
-          {objects.includes("bearings") && (
-            <mesh castShadow receiveShadow geometry={geo("bearings")}>
-              <meshStandardMaterial
-                metalness={1}
-                roughness={0}
-                // map={deckTexture}
-                attach="material"
-                key="metal"
-                toneMapped={false}
-                color={0x999999}
-              />
-            </mesh>
-          )}
-
-          {objects.includes("trucks") && (
-            <mesh castShadow receiveShadow geometry={geo("trucks")}>
-              <meshStandardMaterial
-                metalness={1}
-                roughness={0.2}
-                // map={deckTexture}
-                attach="material"
-                key="metal"
-                toneMapped={false}
-                color={0xcccccc}
-              />
-            </mesh>
-          )}
-        </group>
-      </group>
+    <group>
+      <mesh castShadow receiveShadow geometry={getGeometry("deck-rim")}>
+        <meshStandardMaterial
+          metalness={0}
+          roughness={0.85}
+          map={baseTexture}
+          attach="material"
+          key="rim"
+          toneMapped={false}
+        />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={getGeometry("deck-wrap")}>
+        <meshStandardMaterial
+          {...deckProps}
+          map={deckTexture}
+          attach="material"
+          key="wrap"
+          toneMapped={false}
+        />
+      </mesh>
     </group>
   )
 }
--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,47 +1,91 @@
-import React, { createRef, Suspense } from "react"
+import React, { createRef, Suspense, createContext, useState } from "react"
 import { Canvas } from "@react-three/fiber"
-import Deck from "./Deck"
+import Board from "./Board"
+import SceneContext from "./SceneContext"
 import { boards } from "."
+
 import {
   Environment,
   OrbitControls,
   PerspectiveCamera,
 } from "@react-three/drei"
 
-export const Scene = ({ board, controls, object, texture, scale, objects }) => {
-  const myCamera = createRef()
+export const DEFAULTS = {
+  DECK: { texture: "", metalness: 0, roughness: 0.85 },
+  TRUCKS: { texture: "", metalness: 1, roughness: 0.2, color: 0xcccccc },
+  WHEELS: { texture: "", metalness: 0, roughness: 0.85, color: 0xe3d4ab },
+  BEARINGS: { texture: "", metalness: 1, roughness: 0, color: 0x999999 },
+  RIM: { texture: "", metalness: 0, roughness: 0.85 },
+}
+
+export const DEFAULT = {
+  lighting: {
+    ambient: {
+      intensity: 0.1,
+    },
+    point: {
+      intensity: 0.5,
+      position: [0, 2, 10],
+      penumbra: 1,
+      width: 512,
+      height: 512,
+    },
+  },
+  camera: {
+    position: [0, 5, 5],
+  },
+  objects: {
+    rim: DEFAULTS.RIM,
+    deck: DEFAULTS.DECK,
+  },
+}
+
+export const Scene = ({
+  controls,
+  texture,
+  scale,
+  objects,
+  camera,
+  ambientLight,
+  pointLight,
+}) => {
+  const cameraRef = createRef()
 
   return (
-    <Canvas className="w-full h-full" dispose={null} shadowMap>
-      <ambientLight intensity={0.1} dispose={null} />
-      <pointLight
-        intenstity={0.5}
-        position={[0, 2, 10]}
-        penumbra={1}
-        castShadow
-        shadow-mapSize-height={512}
-        shadow-mapSize-width={512}
-      />
+    <SceneContext.Provider
+      value={{ objects, ambientLight, pointLight, scale, camera }}
+    >
+      <Canvas
+        shadowMap
+        style={{ width: "100%", height: "100%" }}
+        dispose={null}
+      >
+        <ambientLight intensity={ambientLight.intensity} dispose={null} />
 
-      <PerspectiveCamera ref={myCamera} position={[0, 5, 5]} />
-      {controls && <OrbitControls camera={myCamera.current} enableZoom />}
-      <Suspense fallback={null}>
-        <Deck
-          position={[0, 0, 0]}
-          scale={scale || [0.07, 0.07, 0.07]}
-          board={board}
-          boards={boards}
-          object={object}
-          texture={texture}
-          objects={objects}
+        <pointLight
+          intenstity={pointLight.intensity}
+          position={pointLight.position}
+          penumbra={pointLight.penumbra}
+          castShadow
+          shadow-mapSize-height={pointLight.height}
+          shadow-mapSize-width={pointLight.width}
         />
-        <Environment
-          background={false}
-          files={["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]}
-          path="https://sk8verse.xyz/viewer/environment/"
-        />
-      </Suspense>
-    </Canvas>
+
+        <PerspectiveCamera ref={cameraRef} position={camera.position} />
+
+        {controls && <OrbitControls camera={cameraRef.current} enableZoom />}
+
+        <Suspense fallback={null}>
+          <Board />
+
+          <Environment
+            background={false}
+            files={["px.jpg", "nx.jpg", "py.jpg", "ny.jpg", "pz.jpg", "nz.jpg"]}
+            path="https://sk8verse.xyz/viewer/environment/"
+          />
+        </Suspense>
+      </Canvas>
+    </SceneContext.Provider>
   )
 }
 
