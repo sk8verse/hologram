303eb6fccd538b46a49fe5313d288dea4813b922
Beamer <beamer@hey.com>
2021-11-27 11:15:59 -0800

    New layout

--- src/components/Texture.js
+++ src/components/Texture.js
@@ -1,33 +1,43 @@
 const { createCanvas, loadImage } = require("canvas")
 
-const size = 2048
+const canvasSize = 2048
+const deckWidth = 640
 
 export const wrap = async (top, bottom) => {
-  const canvas = createCanvas(size, size)
+  const canvas = createCanvas(canvasSize, canvasSize)
   const ctx = canvas.getContext("2d")
-
-  if (top) {
-    const topImage = await loadImage(top)
-    ctx.drawImage(topImage, 256 + 768, 0, 768, size)
-  }
 
   if (bottom) {
     const bottomImage = await loadImage(bottom)
-    const aspect = bottomImage.height * 0.375
-    console.log(aspect)
+    const aspectWidth = bottomImage.height * 0.3125
 
     ctx.drawImage(
       bottomImage,
-      bottomImage.width / 2 - aspect / 2,
+      bottomImage.width / 2 - aspectWidth / 2,
       0,
-      aspect,
+      aspectWidth,
       bottomImage.height,
-      256,
       0,
-      768,
-      size
+      0,
+      deckWidth,
+      canvasSize
     )
-    // ctx.drawImage(bottomImage, 256, 0, 768, size)
+  }
+
+  if (top) {
+    const topImage = await loadImage(top)
+    const aspectWidth = topImage.height * 0.3125
+    ctx.drawImage(
+      topImage,
+      topImage.width / 2 - aspectWidth / 2,
+      0,
+      aspectWidth,
+      topImage.height,
+      deckWidth,
+      0,
+      deckWidth,
+      canvasSize
+    )
   }
 
   return canvas.toDataURL()
